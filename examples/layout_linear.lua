69c4330b7bb485b6dab08c3ee6b9792e76f01367
Joel Schumacher <joelschum@gmail.com>
2023-06-02 22:31:49 +0200

    Add example cycler

---
+++ examples/layout_linear.lua
@@ -0,0 +1,111 @@
+local jui = require("jui.love").init()
+
+-- A box without a parent has a virtual box the size of the whole window as its parent
+local menu = jui.Box {
+    id = "root",
+    width = jui.pct(100),
+    height = jui.pct(100),
+    children = {
+        jui.Box {
+            id = "staggered",
+            alignx = jui.alignx.center,
+            aligny = jui.aligny.top,
+            height = 120,
+            margin = 20,
+            layout = jui.layout.linear,
+            linearDirection = jui.direction.right,
+            children = {
+                jui.Box { id = "New Game", width = 200, height = 50, margin = 10, aligny = jui.aligny.top },
+                jui.Box { id = "Continue", width = 200, height = 50, margin = 10, aligny = jui.aligny.center },
+                jui.Box { id = "Exit", width = 200, height = 50, margin = 10, aligny = jui.aligny.bottom },
+            }
+        },
+        jui.Box {
+            id = "filled",
+            alignx = jui.alignx.center,
+            aligny = jui.aligny.bottom,
+            width = jui.pct(70),
+            margin = { bottom = 260 },
+            layout = jui.layout.linear,
+            linearDirection = jui.direction.left,
+            spacing = jui.spacing.even,
+            children = {
+                jui.Box { id = "New Game", width = jui.size.fill(2), height = 50, margin = 10 },
+                jui.Box { id = "Continue", width = jui.size.fill, height = 50, margin = 10 },
+                jui.Box { id = "Exit", width = jui.size.fill, height = 50, margin = 10 },
+            }
+        },
+        jui.Box {
+            id = "even",
+            alignx = jui.alignx.center,
+            aligny = jui.aligny.bottom,
+            width = jui.pct(70),
+            margin = { bottom = 180 },
+            layout = jui.layout.linear,
+            linearDirection = jui.direction.left,
+            spacing = jui.spacing.even,
+            children = {
+                jui.Box { id = "New Game", width = 200, height = 50, margin = 10 },
+                jui.Box { id = "Continue", width = 200, height = 50, margin = 10 },
+                jui.Box { id = "Exit", width = 200, height = 50, margin = 10 },
+            }
+        },
+        jui.Box {
+            id = "around",
+            alignx = jui.alignx.center,
+            aligny = jui.aligny.bottom,
+            width = 800,
+            margin = { bottom = 100 },
+            layout = jui.layout.linear,
+            linearDirection = jui.direction.left,
+            spacing = jui.spacing.around,
+            children = {
+                jui.Box { id = "New Game", width = 200, height = 50, margin = 10 },
+                jui.Box { id = "Continue", width = 200, height = 50, margin = 10 },
+                jui.Box { id = "Exit", width = 200, height = 50, margin = 10 },
+            }
+        },
+        jui.Box {
+            id = "between",
+            alignx = jui.alignx.center,
+            aligny = jui.aligny.bottom,
+            width = 800,
+            margin = { bottom = 20 },
+            layout = jui.layout.linear,
+            linearDirection = jui.direction.left,
+            spacing = jui.spacing.between,
+            children = {
+                jui.Box { id = "New Game", width = 200, height = 50, margin = 10 },
+                jui.Box { id = "Continue", width = 200, height = 50, margin = 10 },
+                jui.Box { id = "Exit", width = 200, height = 50, margin = 10 },
+            }
+        },
+        jui.Box {
+            id = "linear down",
+            alignx = jui.alignx.right,
+            aligny = jui.aligny.center,
+            margin = { right = 20 },
+            width = 300,
+            layout = jui.layout.linear,
+            linearDirection = jui.direction.down,
+            -- offset = {x, y} -- manual override
+            -- position = {x, y} -- even harder manual override, but you can use it with percentages
+            children = {
+                jui.Box { id = "New Game", width = 200, height = 50, margin = 10, alignx = jui.alignx.right },
+                jui.Box { id = "Continue", width = 200, height = 50, margin = 10, alignx = jui.alignx.right },
+                jui.Box { id = "Exit", width = 200, height = 50, margin = 10, alignx = jui.alignx.right },
+            }
+        }
+    }
+}
+
+function love.update(dt)
+    jui.update()
+    menu:calculateLayout()
+    --menu:update(love.timer.getTime())
+end
+
+function love.draw()
+    jui.newFrame()
+    menu:draw()
+end
--- main.lua
+++ main.lua
@@ -1,111 +1,73 @@
-local jui = require("jui.love").init()
+local examples = {}
+local currentExampleIndex = 1
 
--- A box without a parent has a virtual box the size of the whole window as its parent
-local menu = jui.Box {
-    id = "root",
-    width = jui.pct(100),
-    height = jui.pct(100),
-    children = {
-        jui.Box {
-            id = "staggered",
-            alignx = jui.alignx.center,
-            aligny = jui.aligny.top,
-            height = 120,
-            margin = 20,
-            layout = jui.layout.linear,
-            linearDirection = jui.direction.right,
-            children = {
-                jui.Box { id = "New Game", width = 200, height = 50, margin = 10, aligny = jui.aligny.top },
-                jui.Box { id = "Continue", width = 200, height = 50, margin = 10, aligny = jui.aligny.center },
-                jui.Box { id = "Exit", width = 200, height = 50, margin = 10, aligny = jui.aligny.bottom },
-            }
+local function runExample(example)
+    love.window.setTitle(example.name .. " - Use ctrl+left / ctrl+right to cycle through")
+    -- use a fresh fenv every time
+    example.fenv = {
+        love = {
+            window = love.window,
+            keyboard = love.keyboard,
+            mouse = love.mouse,
+            joystick = love.joystick,
+            graphics = love.graphics,
         },
-        jui.Box {
-            id = "filled",
-            alignx = jui.alignx.center,
-            aligny = jui.aligny.bottom,
-            width = jui.pct(70),
-            margin = { bottom = 260 },
-            layout = jui.layout.linear,
-            linearDirection = jui.direction.left,
-            spacing = jui.spacing.even,
-            children = {
-                jui.Box { id = "New Game", width = jui.size.fill(2), height = 50, margin = 10 },
-                jui.Box { id = "Continue", width = jui.size.fill, height = 50, margin = 10 },
-                jui.Box { id = "Exit", width = jui.size.fill, height = 50, margin = 10 },
-            }
-        },
-        jui.Box {
-            id = "even",
-            alignx = jui.alignx.center,
-            aligny = jui.aligny.bottom,
-            width = jui.pct(70),
-            margin = { bottom = 180 },
-            layout = jui.layout.linear,
-            linearDirection = jui.direction.left,
-            spacing = jui.spacing.even,
-            children = {
-                jui.Box { id = "New Game", width = 200, height = 50, margin = 10 },
-                jui.Box { id = "Continue", width = 200, height = 50, margin = 10 },
-                jui.Box { id = "Exit", width = 200, height = 50, margin = 10 },
-            }
-        },
-        jui.Box {
-            id = "around",
-            alignx = jui.alignx.center,
-            aligny = jui.aligny.bottom,
-            width = 800,
-            margin = { bottom = 100 },
-            layout = jui.layout.linear,
-            linearDirection = jui.direction.left,
-            spacing = jui.spacing.around,
-            children = {
-                jui.Box { id = "New Game", width = 200, height = 50, margin = 10 },
-                jui.Box { id = "Continue", width = 200, height = 50, margin = 10 },
-                jui.Box { id = "Exit", width = 200, height = 50, margin = 10 },
-            }
-        },
-        jui.Box {
-            id = "between",
-            alignx = jui.alignx.center,
-            aligny = jui.aligny.bottom,
-            width = 800,
-            margin = { bottom = 20 },
-            layout = jui.layout.linear,
-            linearDirection = jui.direction.left,
-            spacing = jui.spacing.between,
-            children = {
-                jui.Box { id = "New Game", width = 200, height = 50, margin = 10 },
-                jui.Box { id = "Continue", width = 200, height = 50, margin = 10 },
-                jui.Box { id = "Exit", width = 200, height = 50, margin = 10 },
-            }
-        },
-        jui.Box {
-            id = "linear down",
-            alignx = jui.alignx.right,
-            aligny = jui.aligny.center,
-            margin = { right = 20 },
-            width = 300,
-            layout = jui.layout.linear,
-            linearDirection = jui.direction.down,
-            -- offset = {x, y} -- manual override
-            -- position = {x, y} -- even harder manual override, but you can use it with percentages
-            children = {
-                jui.Box { id = "New Game", width = 200, height = 50, margin = 10, alignx = jui.alignx.right },
-                jui.Box { id = "Continue", width = 200, height = 50, margin = 10, alignx = jui.alignx.right },
-                jui.Box { id = "Exit", width = 200, height = 50, margin = 10, alignx = jui.alignx.right },
-            }
-        }
+        require = require,
     }
-}
-
-function love.update(dt)
-    jui.update()
-    menu:calculateLayout()
-    --menu:update(love.timer.getTime())
+    setfenv(example.func, example.fenv)
+    example.func()
+    if example.fenv.love.load then
+        example.fenv.love.load()
+    end
 end
 
-function love.draw()
-    jui.newFrame()
-    menu:draw()
+local function currentExample()
+    return examples[currentExampleIndex]
+end
+
+function love.load()
+    -- keypressed is special, because we need to intercept it and load is special, because we need to do it on load
+    local callbacks = {
+        "draw", "update", "focus", "resize", "keyreleased", "mousemoved", "mousepressed",
+        "mousereleased", "wheelmoved", "gamepadaxis", "gamepadpressed", "gamepadreleased",
+    }
+    for _, callback in ipairs(callbacks) do
+        love[callback] = function(...)
+            if currentExample().fenv.love[callback] then
+                currentExample().fenv.love[callback](...)
+            end
+        end
+    end
+
+    local exampleFiles = love.filesystem.getDirectoryItems("examples/")
+    table.sort(exampleFiles)
+    for _, file in ipairs(exampleFiles) do
+        local example = {
+            name = file,
+            func = love.filesystem.load("examples/" .. file),
+        }
+        table.insert(examples, example)
+    end
+    runExample(currentExample())
+end
+
+function love.keypressed(key, ...)
+    local ctrl = love.keyboard.isDown("lctrl") or love.keyboard.isDown("rctrl")
+    if ctrl and key == "left" then
+        currentExampleIndex = currentExampleIndex - 1
+        if currentExampleIndex < 1 then
+            currentExampleIndex = #examples
+        end
+        runExample(currentExample())
+    elseif ctrl and key == "right" then
+        currentExampleIndex = currentExampleIndex + 1
+        if currentExampleIndex > #examples then
+            currentExampleIndex = 1
+        end
+        runExample(currentExample())
+    end
+
+    if currentExample().fenv.love.keypressed then
+        currentExample().fenv.love.keypressed(key, ...)
+    end
 end
